fc49d3529e27acc8957c43dd5187a16f0d9f6610
Richard Harris <richardharris916@gmail.com>
2021-02-07 03:15:08 -0600

    Added example of usage for hook

--- example/webpack.config.js
+++ example/webpack.config.js
@@ -21,11 +21,11 @@ module.exports = {
          {
             test: /\.css$/,
             use: ['style-loader', 'css-loader'],
-         },
-         {
-            test: /\.(png|j?g|svg|gif)?$/,
-            use: 'file-loader'
-         }
+         }//,
+         // {
+         //    test: /\.(png|j?g|svg|gif)?$/,
+         //    use: 'file-loader'
+         // }
 ]
    },
    plugins: [
